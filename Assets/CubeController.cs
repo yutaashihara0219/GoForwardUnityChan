e3b3ee1c932df7205aae52bbb365946a97fe6982
yutaashihara0219 <ashihara0219@gmail.com>
2025-03-02 19:33:19 +0900

    ブロックの効果音の追加
    GroundとCubeのタグを追加し、CubePrefabと接触したときに効果音が鳴るようスクリプト作成とアタッチを行った。
    効果音は２つ。

--- Assets/CubeController.cs
+++ Assets/CubeController.cs
@@ -10,11 +10,19 @@ public class CubeController : MonoBehaviour
     // 消滅位置
     private float deadLine = -10;
 
+    // 地面とぶつかった時の音
+    public AudioClip se1;
+    // 地面とぶつかった時の音
+    public AudioClip se2;
+    // AudioClip再生用
+    AudioSource audioSource;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // オーディオのコンポーネントを取得
+        this.audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -27,6 +35,23 @@ public class CubeController : MonoBehaviour
         if (transform.position.x < this.deadLine)
         {
             Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //地面と衝突した場合
+        if(collision.gameObject.tag == "Ground")
+        {
+            //オーディオ再生
+            this.audioSource.PlayOneShot(se1);
+        }
+
+        //キューブ同士で衝突した場合
+        if (collision.gameObject.tag == "Cube")
+        {
+            //オーディオ再生
+            this.audioSource.PlayOneShot(se2);
         }
     }
 }
